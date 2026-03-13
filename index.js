211823561a9686825bdc183a710adf669bb0c6db
Shravan Vignesh <shravanvignesh2003@gmail.com>
2026-03-13 05:09:43 +0000

    test files cleanup

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/', (req, res) => {
-  res.send('✅ Node + Express + GitHub Desktop are all working!');
-});
-
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
